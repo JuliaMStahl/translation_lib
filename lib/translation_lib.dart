b38f829028e29e6631de4f79e902ce1728341214
Julia Stahl <julia.stahl@kepha.com.br>
2023-01-30 11:29:31 -0300

    Adiciona arquivos de tradução de acesso, dashboard e perfil

--- lib/translation_lib.dart
+++ lib/translation_lib.dart
@@ -1,7 +1,22 @@
 library translation_lib;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:acesso/generated/locales.g.dart' as acesso_locales;
+import 'package:dashboard/generated/locales.g.dart' as dashboard_locales;
+import 'package:perfil/generated/locales.g.dart' as perfil_locales;
+
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    'pt_BR': Locales.pt_BR,
+  };
+}
+
+class Locales {
+  static final Map<String, String> _pt_BR = {};
+
+  static Map<String, String> get pt_BR {
+    _pt_BR.addAll(acesso_locales.Locales.pt_BR);
+    _pt_BR.addAll(dashboard_locales.Locales.pt_BR);
+    _pt_BR.addAll(perfil_locales.Locales.pt_BR);
+    return _pt_BR;
+  }
 }
